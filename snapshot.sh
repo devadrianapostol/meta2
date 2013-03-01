a6758617e29c626f04f36e18703ec056dc1aab93
felix <felix@call-with-current-continuation.org>
2013-03-01 23:57:16 +0100

    bash quoting rules drive me mad

--- snapshot.sh
+++ snapshot.sh
@@ -8,9 +8,11 @@ set -e
 # check for changes
 if git diff --quiet; then :
 else
-    echo "There are still uncommited changes in the working directory."
-    echo "Please commit them first before generating a snapshot."
-    exit 1
+    if test "$1" \!= "-n"; then
+	echo "There are still uncommited changes in the working directory."
+	echo "Please commit them first before generating a snapshot."
+	exit 1
+    fi
 fi
 
 # obtain HEAD id
@@ -23,10 +25,13 @@ make -f Makefile all check
 # generate bootstrap source
 echo '#define FINGERPRINT "'$fingerprint'"' >bootstrap/meta2.c
 echo '#define GENEALOGY \' >>bootstrap/meta2.c
-bootstrap/meta2 -g >>bootstrap/meta2.c
+
+for v in $(bootstrap/meta2 -g); do
+    echo '"'$v'\\n" \' >>bootstrap/meta2.c
+done
+
 parent=$(bootstrap/meta2 -v)
-echo '"\"'$parent'\\n\"" \' >>bootstrap/meta2.c
-echo '""' >>bootstrap/meta2.c
+echo '"'$parent'\\n"' >>bootstrap/meta2.c
 cat meta2.h meta2.c >>bootstrap/meta2.c
 
 # regenerate meta2.c
@@ -37,4 +42,3 @@ if test "$1" \!= "-n"; then
     echo "committing snapshot"
     git commit -m "snapshot" bootstrap/meta2.c
 fi
-
